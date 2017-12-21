1b9d9703d38a49f5019091133bed9d8b2f2b55c2
manderson18 <ma128@pvsd.org>
2017-12-21 08:40:41 -0500

    we

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	//TIC TAC TOE BOARD
 
-        System.out.println("-------------------------------");
-        System.out.println("|                         |");
-        System.out.println("-------------------------------");
-        System.out.println("|                         |");
-        System.out.println("-------------------------------");
-        System.out.println("|                         |");
+        System.out.println("|        |          |         |");
+        System.out.println("|-----------------------------|");
+        System.out.println("|        |          |         |");
+        System.out.println("|-----------------------------|");
+        System.out.println("|        |          |         |");
 
     }
 }
